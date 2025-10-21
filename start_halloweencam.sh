9b2339a54c93c2c968b092928dfcf884c52d33fd
Your Name <you@example.com>
2025-10-20 20:52:46 -0700

    script

--- start_halloweencam.sh
+++ start_halloweencam.sh
@@ -27,6 +27,11 @@ source "$PROJECT_DIR/venv/bin/activate"
 echo "Running pip install..."
 pip install -e .
 
+# Use MJPEG @ 640x360 60 FPS (supported by your cam)
+v4l2-ctl --set-fmt-video=width=640,height=360,pixelformat=MJPG
+v4l2-ctl --set-parm=30
+
+
 echo "Launching ghoulfluids GUI..."
 # Explicitly set the display for GUI applications
 export DISPLAY=:0
