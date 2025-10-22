65954dcca7306198b3e7f6c29e68ecf40454f619
rcompton <ryan@ryancompton.net>
2025-10-21 20:23:39 -0700

    win = glfw.create_window(cfg.width, cfg.height, "Ghoul Fluids", monitor, None)     glfw.make_context_current(win)     glfw.swap_interval(0)  # <-- Add this line to disable VSync

--- src/ghoulfluids/app.py
+++ src/ghoulfluids/app.py
@@ -145,6 +145,7 @@ def main(argv=None):
 
     win = glfw.create_window(cfg.width, cfg.height, "Ghoul Fluids", monitor, None)
     glfw.make_context_current(win)
+    glfw.swap_interval(0)  # disable VSync
 
     def _linux_gl_hint():
         if sys.platform.startswith("linux"):
